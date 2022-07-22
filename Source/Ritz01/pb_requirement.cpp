1da5997eb7216af50e145cc73518051122e1ebe6
Luciobio <pardobio@gmail.com>
2022-07-22 17:47:27 -0300

    All PW requirements ready

---
+++ Source/Ritz01/pb_requirement.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "pb_requirement.h"
+
+pb_requirement::pb_requirement()
+{
+}
+
+pb_requirement::~pb_requirement()
+{
+}
---
+++ Source/Ritz01/pb_requirement.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class RITZ01_API pb_requirement
+{
+public:
+	pb_requirement();
+	~pb_requirement();
+};
